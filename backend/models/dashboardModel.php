f121a9350b1317575ca2a67372a59e75f71b9704
Nícolas Marcelo Lima Eloy <134300551+nicolasmleloy@users.noreply.github.com>
2025-12-03 16:59:29 -0400

    fix/ corrigido erro de NaN nos dados da dashboard

--- backend/models/dashboardModel.php
+++ backend/models/dashboardModel.php
@@ -9,6 +9,10 @@ class DashboardModel {
     public function __construct() {
         $banco = new Database();
         $this->conn = $banco->Connect();
+
+        if ($this->conn === null) {
+            die("ERRO: Não foi possível conectar ao banco.");
+        }
     }
 
     public function getDashboardData() {
@@ -18,7 +22,7 @@ class DashboardModel {
 
         $sql = "SELECT COUNT(*) AS servicos_concluidos 
                 FROM orcamento 
-                WHERE status = 'aprovado'";
+                WHERE status_orcamento = 'aprovado'";
         $servicosConcluidos = $this->conn->query($sql)->fetch(PDO::FETCH_ASSOC);
 
         $sql = "
@@ -27,7 +31,7 @@ class DashboardModel {
                 SUM(oi.valor) AS total_servico
             FROM orcamento o
             LEFT JOIN orcamento_itens oi ON oi.orcamento_id = o.id
-            WHERE o.status = 'aprovado'
+            WHERE o.status_orcamento = 'aprovado'
             GROUP BY o.id
         ";
         $ticketMedioLista = $this->conn->query($sql)->fetchAll(PDO::FETCH_ASSOC);
